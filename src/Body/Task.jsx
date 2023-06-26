b31a61cc71639ca07fca8916c0575abfd4bb0702
Andy Frievalt <afrievalt@yahoo.com>
2023-06-26 07:23:41 -0500

    add filters folder

--- src/Body/Task.jsx
+++ src/Body/Task.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 
+
 function Task(props) {
-  const { name, isCompleted, onClick } = props;
+  const { name, isCompleted } = props;
 
   return (
     <li>
       <label>
-        <input type="checkbox" checked={isCompleted} onChange={onClick} />
+        <input type="checkbox" checked={isCompleted}  />
         {name}
       </label>
     </li>
   );
 }
+
+/*
+Note:
+This code will generate a warning:
+Warning: You provided a `checked` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultChecked`. Otherwise, set either `onChange` or `readOnly`.
+
+This issue will be resolved in the next step.
+*/
 
 export default Task;
